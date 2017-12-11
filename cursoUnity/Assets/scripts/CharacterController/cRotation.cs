8b9e9edf212f32783f23d824d359e76a56c873de
Miguel Angel González Hernández <migueljabugo@gmail.com>
2017-12-11 20:51:19 +0100

    no message

---
+++ cursoUnity/Assets/scripts/CharacterController/cRotation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cRotation : MonoBehaviour {
+
+	float mouseX;
+	float mouseY;
+	public GameObject camera;
+
+	void Update () {
+		//Rotation
+		mouseX = Input.GetAxis ("mouseX");
+		mouseY = Input.GetAxis ("mouseY");
+
+		if (mouseX>0 || mouseX<0) {
+			transform.Rotate (new Vector3(0, mouseX, 0));
+		}
+
+		if (mouseY>0 || mouseY<0) {
+			camera.transform.Rotate (new Vector3(mouseY, 0, 0));
+		}
+	}
+}
+
+
