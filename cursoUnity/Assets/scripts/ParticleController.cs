301b094a587bcd60b2aedd4bab0c94f2c41765db
Miguel Angel González Hernández <migueljabugo@gmail.com>
2017-12-18 17:47:37 +0100

    Particulas

---
+++ cursoUnity/Assets/scripts/ParticleController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour {
+
+	public ParticleSystem particle;
+
+	void Start () {
+		particle = GetComponent<ParticleSystem> ();
+		//particle.Play ();
+	}
+
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.A)) {
+			particle.Play ();
+		}else if(Input.GetKeyDown(KeyCode.B)){
+			particle.Stop ();
+		}
+	}
+}
